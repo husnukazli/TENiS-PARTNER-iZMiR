eafe761bc38a9a829d979afe31e1b8919cebf2f4
husnukazli <husnukazli@gmail.com>
2026-07-21 10:05:04 +0300

    Update tenis_partner_havuzu_3.py

--- tenis_partner_havuzu_3.py
+++ tenis_partner_havuzu_3.py
@@ -34,7 +34,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Çerez Yöneticisini Başlat (Beni Hatırla özelliği için)
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_manager():
     return stx.CookieManager()
 
